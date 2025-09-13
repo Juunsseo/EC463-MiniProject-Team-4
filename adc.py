7a1547cb66b656697bf6a40037706cea19e82b59
Jessica Ramirez <ramirez2@bu.edu>
2025-09-13 13:34:26 -0400

    adc and buzzer functionality

--- adc.py
+++ adc.py
@@ -1,1 +1,32 @@
+# drivers/adc.py
+import machine
 
+# Initialize ADC on GP28 (ADC2)
+adc = machine.ADC(28)
+
+#Calibrated Values
+RAW_MIN = 600      # Dark
+RAW_MAX = 65338    # Flashlight
+
+def read_sensor_raw() -> int:
+    """
+    Read raw ADC from the photoresistor.
+    Returns 16-bit value.
+    """
+    return adc.read_u16()
+
+def normalize_raw(raw: int, min_in: RAW_MIN, max_in: RAW_MAX) -> float:
+    """
+    Normalize raw ADC value to a float in [0.0, 1.0].
+    Returns: float in [0.0, 1.0]
+    """
+    if raw < min_in:
+        raw = min_in
+    elif raw > max_in:
+        raw = max_in
+    return (raw - min_in) / (max_in - min_in)
+
+def estimate_lux(norm: float) -> float:
+  """Rough estimate of lux  from normalized value."""
+    # Simple linear mapping: 0.0 → 0 lux, 1.0 → 1000 lux
+    return norm * 1000
--- buzzer.py
+++ buzzer.py
@@ -1,1 +1,27 @@
+# drivers/buzzer.py
+import machine
+
+# Setup PWM on GP16
+_buzzer_pin = machine.Pin(16)
+_pwm = machine.PWM(_buzzer_pin)
+_pwm.duty_u16(0)  # initially silent
+
+def start_tone(freq: int, duty: float = 0.5) -> None:
+    """
+    Start playing a tone at given frequency and duty cycle.
+    Args:
+        freq: frequency in Hz
+        duty: duty cycle [0.0, 1.0]
+    """
+    if freq <= 0:
+        stop_tone()
+        return
+    _pwm.freq(freq)
+    duty_val = int(65535 * duty)
+    _pwm.duty_u16(duty_val)
+
+def stop_tone() -> None:
+    """Stop playing sound (set duty to 0)."""
+    _pwm.duty_u16(0)
+
 
